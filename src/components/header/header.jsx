6f0a8c82875fbc067c5e5d5a662108b2a79f4bd7
florin <fofo.munteanu@gmail.com>
2024-06-02 23:56:13 +0300

    style

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -14,7 +14,7 @@ function DisplayNavBar() {
                 <label htmlFor="nav"></label>
           <ul className="ul-list">
             <li className="list-item">
-              <a href="/">Acasa beta</a>
+              <a href="/">Acasa main</a>
             </li>
             <li className="list-item">
               <a href="#meniu">Meniu</a>
